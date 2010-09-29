89672cd192404e5027017faa490b65b15069e7cf
Gary Rudolph <GaryRudolph@me.com>
2010-09-28 19:45:50 -0700

    Removed shell scripts

--- locations.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# -v for in/out
-curl -i -X GET http://api.citygridmedia.com/search/locations?where=computrparts\&where=90069\&publisher=acme\&format=xml
-
-#/search/locations?type=movietheater&where=90045
